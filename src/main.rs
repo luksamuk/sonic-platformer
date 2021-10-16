4bfcefe1534b7eb33bdb04758a9b4e3fd5f767e6
Lucas S. Vieira <lucasvieira@protonmail.com>
2021-10-16 13:10:26 -0300

    Adiciona exemplo básico com ggez

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use ggez::event;
+use ggez::graphics;
+use ggez::{Context, GameResult};
+use glam::*;
+
+struct MainState{}
+
+impl MainState {
+    fn new() -> GameResult<MainState> {
+        Ok(MainState {})
+    }
+}
+
+impl event::EventHandler<ggez::GameError> for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::clear(ctx, [0.0, 0.0, 0.0, 1.0].into());
+
+        let circulo = graphics::Mesh::new_circle(
+            ctx,
+            graphics::DrawMode::fill(),
+            Vec2::new(0.0, 0.0),
+            100.0,
+            0.001,
+            graphics::Color::WHITE,
+        )?;
+        graphics::draw(ctx, &circulo, (Vec2::new(200.0, 380.0),))?;
+
+        graphics::present(ctx)?;
+        Ok(())
+    }
+}
+
+fn main() -> GameResult {
+    let context_builder = ggez::ContextBuilder::new(
+        "platformer", "Lucas S. Vieira");
+    let (ctx, event_loop) = context_builder.build()?;
+    let state = MainState::new()?;
+    event::run(ctx, event_loop, state)
 }
