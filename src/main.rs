2f91d4b04ccb7f4a8f4f0e6108771bfa115aec6c
Lucas S. Vieira <lucasvieira@protonmail.com>
2021-10-31 16:11:15 -0300

    Suporte inicial para telas separadas e menu principal

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,25 @@
-use ggez::event;
-use ggez::graphics;
-use ggez::{Context, GameResult};
-use glam::*;
+use ggez::{
+    conf::{FullscreenType, WindowMode, WindowSetup},
+    event, ContextBuilder, GameResult,
+};
+use sonic_platformer::*;
 
-struct MainState {
-    circulo_x: f32,
-}
-
-impl MainState {
-    fn new() -> GameResult<MainState> {
-        let state = MainState { circulo_x: 0.0 };
-        Ok(state)
-    }
-}
-
-impl event::EventHandler<ggez::GameError> for MainState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        self.circulo_x = self.circulo_x % 800.0 + 1.0;
-        Ok(())
-    }
-
-    fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        graphics::clear(ctx, [0.0, 0.0, 0.0, 1.0].into());
-
-        let circulo = graphics::Mesh::new_circle(
-            ctx,
-            graphics::DrawMode::fill(),
-            Vec2::new(0.0, 0.0),
-            100.0,
-            0.001,
-            graphics::Color::WHITE,
-        )?;
-        graphics::draw(ctx, &circulo, (Vec2::new(self.circulo_x, 380.0),))?;
-
-        graphics::present(ctx)?;
-        Ok(())
-    }
-}
+const GAME_NAME: &str = "Platformer";
 
 fn main() -> GameResult {
-    let context_builder = ggez::ContextBuilder::new("platformer", "Lucas S. Vieira");
-    let (ctx, event_loop) = context_builder.build()?;
-    let state = MainState::new()?;
-    event::run(ctx, event_loop, state)
+    let window_setup = WindowSetup::default().title(GAME_NAME).vsync(false);
+
+    let window_mode = WindowMode::default()
+        //.dimensions(1280.0, 720.0)
+        //.fullscreen_type(FullscreenType::Desktop)
+        .dimensions(854.0, 480.0)
+        .resizable(false);
+
+    let (ctx, event_loop) = ContextBuilder::new("sonic_platformer", "Lucas S. Vieira")
+        .window_mode(window_mode)
+        .window_setup(window_setup)
+        .build()?;
+
+    let main_state = MainState::new(GAME_NAME)?;
+    event::run(ctx, event_loop, main_state)
 }
