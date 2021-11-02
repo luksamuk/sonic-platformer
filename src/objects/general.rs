0dda8fdd8dfb27c46636f286692a166dc0747b02
Lucas S. Vieira <lucasvieira@protonmail.com>
2021-11-02 12:43:26 -0300

    Adiciona documentação

--- src/objects/general.rs
+++ src/objects/general.rs
@@ -1,8 +1,16 @@
 use glam::Vec2;
 
+/// Refers to a tag component which can be attached to any entity.
+///
+/// This is a multi-purpose tag which can be used as a flag or as
+/// an extra generic value.
 #[derive(Default, Clone, Copy, Debug, PartialEq)]
 pub struct Tag(pub u64);
 
+/// Refers to a position component.
+///
+/// Should be used with any entity that has a position on the
+/// 2D plane.
 #[derive(Default, Clone, Copy, Debug, PartialEq)]
 pub struct Position(pub Vec2);
 
