a5859800e41866de0986cf3a9f124801b3a54bb1
Lucas S. Vieira <lucasvieira@protonmail.com>
2021-11-09 17:51:33 -0300

    Adiciona lógica inicial de renderização de tiles (em progresso)

--- src/objects/level/mod.rs
+++ src/objects/level/mod.rs
@@ -8,8 +8,39 @@ pub struct Tile16 {
     pub angle: f32,
 }
 
+impl Tile16 {
+    pub fn draw(&self, context: &mut Context, sheet: &SpriteAtlas, hotspot: Vec2, camera_pos: Vec2) -> GameResult {
+        let scale = glam::vec2(1.0, 1.0);
+        let mut i = 0;
+        for tile in self.tiles {
+            let position = glam::vec2(
+                (i % 2) as f32 * 8.0,
+                (i / 2.0).floor() * 8.0,
+            ) + hotspot;
+            let position = position - camera_pos;
+            sheet.draw(context, tile, position, scale)?;
+            i += 1;
+        }
+        Ok(())
+    }
+}
+
 pub struct Tile128 {
     pub tiles: Vec<u64>,
+}
+
+impl Tile128 {
+    pub fn draw(&self, context: &mut Context, sheet: &SpriteAtlas, hotspot: Vec2, camera_pos: Vec2) -> GameResult {
+        let mut i = 0;
+        for tile in self.tiles {
+            let position = glam::vec2(
+                (i % 2) as f32 * 16.0,
+                (i / 2.0).floor() * 16.0,
+            ) + hotspot;
+            tile.draw(context, sheet, position, camera_pos)?;
+        }
+        Ok(())
+    }
 }
 
 pub struct Map {
